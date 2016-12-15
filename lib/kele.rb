672a6f8ecb01b3b798d55b2a20a4acf0dc8f845b
Jason Clegg <jgcdeveloper@gmail.com>
2016-12-15 21:21:59 +0100

    Refactor Code for DRYness

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,22 +2,23 @@ require 'httparty'
 
 class Kele
 
+  attr_reader :user_token
+
   include HTTParty
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email,password)
+
     options = {
       body: {
-        "email": "#{email}",
-        "password": "#{password}"
+        email: email,
+        password: password
       }
     }
 
-    @auth_token = self.class.post('/sessions', options).first.last
+    @user_token = self.class.post('/sessions', options).first.last
+
   end
 
-  def user_token
-    @auth_token
-  end
 
 end
