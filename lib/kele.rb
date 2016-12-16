fdcdb999bffc9241f3ab4c4def805b96340da373
Jason Clegg <jgcdeveloper@gmail.com>
2016-12-17 00:07:10 +0100

    Add method get_me to return current user info from Bloc API

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,11 +1,13 @@
 require 'httparty'
+require 'json'
 
 class Kele
 
-  attr_reader :response, :auth_token
+  attr_reader :http_response, :auth_token, :current_user
 
   include HTTParty
 
+  #This method will execute when the class is instansiated
   def initialize(email,password, base_url = 'https://www.bloc.io/api/v1')
 
     self.class.base_uri base_url
@@ -18,10 +20,35 @@ class Kele
     }
 
     post = self.class.post('/sessions', options)
-    @response = post.code
+    @http_response = post.code
     @auth_token = post["auth_token"]
 
   end
 
+  #This method will retrieve the user information from the API, strip the headers,
+  #and convert into a ruby hash form. It will then store that into @current_user
+  #and also return it from the method.
+
+  def get_me
+
+    if (@auth_token)
+      puts "Setting user data into @current_user"
+
+      #Using our authorization token to retrieve the current user
+      response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+
+      #Taking the API response and converting the body to a ruby hash
+      @current_user = JSON.parse(response.body)
+
+    else
+
+      puts "Cannot retrieve current user without a valid authorization token"
+
+      #With an invalid token, @current_user will be set to nil
+      @current_user = nil
+
+    end
+
+  end
 
 end
--- spec/lib/kele_spec.rb
+++ spec/lib/kele_spec.rb
@@ -1,24 +1,24 @@
 RSpec.describe Kele do
 
+  let(:kele_authorized) { Kele.new( ENV['BLOC_EMAIL'], ENV['BLOC_PASSWORD'],'https://private-anon-840be6fd6b-blocapi.apiary-proxy.com/api/v1')}
   let(:kele_unauthorized) { Kele.new("antblessing@gmail.com","password",'https://private-anon-840be6fd6b-blocapi.apiary-proxy.com/api/v1')}
-  let(:kele_authorized) { Kele.new(ENV["BLOC_EMAIL"],ENV["BLOC_PASSWORD"],'https://private-anon-840be6fd6b-blocapi.apiary-proxy.com/api/v1')}
 
-  describe "HTTP Response Codes" do
+  describe "Testing HTTP Response Codes" do
 
     it "Returns 200 for an accepted connection with accepted credentials" do
-      expect(kele_authorized.response).to eq(200)
+      expect(kele_authorized.http_response).to eq(200)
     end
 
     it "Returns 403 for an improper login credentials" do
-      expect(kele_unauthorized.response).to eq(403)
+      expect(kele_unauthorized.http_response).to eq(403)
     end
 
   end
 
-  describe "attributes" do
+  describe "Testing attributes" do
 
-    it "Has an auth_token and response attribute" do
-      expect(kele_authorized).to have_attributes(auth_token: kele_authorized.auth_token, response: kele_authorized.response)
+    it "Has an http_response, auth_token and current_user attributes" do
+      expect(kele_authorized).to have_attributes(auth_token: kele_authorized.auth_token, http_response: kele_authorized.http_response, current_user: kele_authorized.current_user)
     end
 
     it "Provides an auth_token for an authorized user" do
@@ -27,6 +27,40 @@ RSpec.describe Kele do
 
     it "Does not provide auth_token for an unauthorized user" do
       expect(kele_unauthorized.auth_token).to be_nil
+    end
+
+  end
+
+  describe "Test calling get_me without a valid topic" do
+
+    before(:example) do
+      kele_unauthorized.get_me
+    end
+
+    it "Will not write data to @current_user without a authorization token" do
+      expect(kele_unauthorized.current_user).to be_nil
+    end
+
+  end
+
+  describe "Testing the get_me method with a valid token" do
+
+    before(:example) do
+      kele_authorized.get_me
+    end
+
+    it "Will set data to @current_user with an authorization token" do
+      expect(kele_authorized.current_user).to_not be_nil
+    end
+
+    it "Has several of the expected hash keys in the returned body" do
+      expect(kele_authorized.current_user["id"]).to_not be_nil
+      expect(kele_authorized.current_user["name"]).to_not be_nil
+      expect(kele_authorized.current_user["email"]).to_not be_nil
+    end
+
+    it "Does not find an invalid hash key" do
+      expect(kele_authorized.current_user["invalid"]).to be_nil
     end
 
   end
